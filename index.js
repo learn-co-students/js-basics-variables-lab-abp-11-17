bdfd84a9b73302ed4ede3ebb50b2fa2ca769721d
George Eckel <george_eckel@intuit.com>
2017-11-10 18:17:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
-const companyName = 'specialCompany';
+
 var mostProfitableNeighborhood = 'Chelsea';
-var mostProfitableNeighborhood = 'Upper West Side';
+
 var companyCeo ='Susan Smith';
-var companyCeo = 'Lauren Hart';
