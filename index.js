4c8f0005a64e91bba714b1b0206e0c165cf0ab6a
evaparicio Evelyn <ev.aparicio.c@gmail.com>
2017-11-12 00:08:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const companyName = 'Scuber';
 let mostProfitableNeighborhood = "Chelsea";
+<<<<<<< HEAD
 let companyCeo = "Susan Smith";
+=======
+let companyCeo = "Susan Smith";
+>>>>>>> 538899e9a66a3507ce64a7f7e7e64c24eed4f95e
