ed02148ba7190dc2be22d128d36e0214cdfeeeb6
James Yu <jamesyu6149@gmail.com>
2017-11-14 06:55:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
 const companyName = 'Scuber';
+
 let mostProfitableNeighborhood = 'Chelsea';
-let mostProfitableNeighborhood = 'Upper West Side';
+
 let companyCeo = 'Susan Smith';
-let companyCeo = 'Laudren Hart';
