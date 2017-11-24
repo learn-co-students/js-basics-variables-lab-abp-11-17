b17084ac11b53a6c50f5c7bc688904ad9a5e6d63
Erika Pingatore <ping.erika@gmail.com>
2017-11-24 21:29:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+
+
+
+let mostProfitableNeighborhood = 'Chelsea';
+  mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
+  companyCeo = 'Susan Smith';
+
+const companyName = 'Scuber';
+    companyName = 'Scuber';
